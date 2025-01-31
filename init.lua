054b573bd231d513f2f74479032bdff19dbe6fad
rafatokairin <rptokairin@gmail.com>
2025-01-30 23:26:27 -0300

    vers 2

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-vim.cmd("set mouse=r")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -19,22 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
 require("lazy").setup("plugins")
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-
-local config = require("nvim-treesitter.configs")
-
-config.setup({
-  ensure_installed = { "c", "lua", "javascript" },
-  highlight = { enable = true },
-  indent = { enable = true },  
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
